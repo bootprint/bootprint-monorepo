85364478c0ec56cb3bea99b2ed80fab607aae6cc
Nils Knappmeier <n.knappmeier@i-views.de>
2015-06-23 20:41:59 +0200

    Initial check-in

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * ride-over <https://github.com/nknapp/ride-over>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function rideOver () {
+  // body
+}
---
+++ spec/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * ride-over <https://github.com/nknapp/ride-over>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var rideOver = require('../')
+
+describe('ride-over:', function () {
+  // body
+})
