dd11746590c83fc2ec7753bba2ebf0a0d7e525bd
Nils Knappmeier <n.knappmeier@i-views.de>
2015-06-28 23:13:24 +0200

    Initial check-in

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * customize-engine-handlebars <https://github.com/nknapp/customize-engine-handlebars>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function customizeEngineHandlebars () {
+  // body
+}
---
+++ spec/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * customize-engine-handlebars <https://github.com/nknapp/customize-engine-handlebars>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var customizeEngineHandlebars = require('../')
+
+describe('customize-engine-handlebars:', function () {
+  // body
+})
