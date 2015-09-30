abc6e02d37211b389efe6a9cc30bc75dfdc5cc0f
Nils Knappmeier <n.knappmeier@i-views.de>
2015-09-30 13:00:37 +0200

    Initial check-in

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * customize-engine-less <https://github.com/nknapp/customize-engine-less>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function customizeEngineLess () {
+  // body
+}
---
+++ test/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * customize-engine-less <https://github.com/nknapp/customize-engine-less>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var customizeEngineLess = require('../')
+
+describe('customize-engine-less:', function () {
+  // body
+})
