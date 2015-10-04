d5f2bcbe36a4f72ab9bbf7637f4f8b0dc79aacaa
Nils Knappmeier <n.knappmeier@i-views.de>
2015-10-04 08:06:14 +0200

    Initial check-in

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * customize-watch <https://github.com/nknapp/customize-watch>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function customizeWatch () {
+  // body
+}
---
+++ test/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * customize-watch <https://github.com/nknapp/customize-watch>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var customizeWatch = require('../')
+
+describe('customize-watch:', function () {
+  // body
+})
