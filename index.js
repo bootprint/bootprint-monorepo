3c6fcee549d7428fda400b53d444db118a3982a9
Nils Knappmeier <n.knappmeier@i-views.de>
2015-10-09 10:36:48 +0200

    Initial check-in

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * customize-write-files <https://github.com/nknapp/customize-write-files>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function customizeWriteFiles () {
+  // body
+}
---
+++ test/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * customize-write-files <https://github.com/nknapp/customize-write-files>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var customizeWriteFiles = require('../')
+
+describe('customize-write-files:', function () {
+  // body
+})
