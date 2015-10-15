24c276ab822b85a185a8deea074abcd17d2f36c5
Nils Knappmeier <n.knappmeier@i-views.de>
2015-10-15 09:13:59 +0200

    Remove "console.log"

--- handlebars/helpers.js
+++ handlebars/helpers.js
@@ -93,7 +93,6 @@ module.exports = {
      * @returns {boolean}
      */
     'equal': function(value1,value2) {
-        console.log(value1,value2,value1==value2);
         return value1 == value2;
     },
     /**
