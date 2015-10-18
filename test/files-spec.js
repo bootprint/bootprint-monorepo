0b01ee6ac853bc8a5f189245c8ead9b8c781aa4b
Nils Knappmeier <n.knappmeier@i-views.de>
2015-10-19 00:24:40 +0200

    Update dependency versions + testcase

--- test/files-spec.js
+++ test/files-spec.js
@@ -43,4 +43,27 @@ describe('the files-function', function () {
         expect(x.valueOf()['zwei.hbs'].inspect().state).to.equal('pending')
       })
   })
+
+  it('should work correctly with globs', function () {
+    return deep(
+      _.merge(
+        {dir: x = files('test/fixtures/testPartials1', { glob: '*ei.hbs' })},
+        {dir: files('test/fixtures/testPartials2', { glob: '*ei.hbs' })},
+        overrider)
+    )
+      .then(function (result) {
+        expect(result).to.eql({
+          dir: {
+            'zwei.hbs': {
+              path: 'test/fixtures/testPartials2/zwei.hbs',
+              contents: 'testPartials2/zwei {{zwei}}'
+            },
+            'drei.hbs': {
+              path: 'test/fixtures/testPartials2/drei.hbs',
+              contents: 'testPartials2/drei {{drei}}'
+            }
+          }
+        })
+      })
+  })
 })
