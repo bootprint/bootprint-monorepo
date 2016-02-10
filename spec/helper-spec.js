9bf386c09d4dd7f9aa37c82e5c8d7aa8574da748
Nils Knappmeier <n.knappmeier@i-views.de>
2016-02-10 17:13:24 +0100

    Apply StandardJS formatting

--- spec/helper-spec.js
+++ spec/helper-spec.js
@@ -1,15 +1,15 @@
 /* global describe */
 /* global it */
 /* global expect */
-var helpers = require("../handlebars/helpers.js");
-describe("the 'htmlId'-helper'", function() {
-    it("should replace illegal characters with '-'", function() {
-        expect(helpers.htmlId(";/abc")).toBe("--abc");
-    })
-});
+var helpers = require('../handlebars/helpers.js')
+describe("the 'htmlId'-helper'", function () {
+  it("should replace illegal characters with '-'", function () {
+    expect(helpers.htmlId(';/abc')).toBe('--abc')
+  })
+})
 
-describe("the 'toUpperCase'-helper'", function() {
-    it("should convert strings to upper-case", function() {
-        expect(helpers.toUpperCase("abc")).toBe("ABC");
-    })
-});
+describe("the 'toUpperCase'-helper'", function () {
+  it('should convert strings to upper-case', function () {
+    expect(helpers.toUpperCase('abc')).toBe('ABC')
+  })
+})
