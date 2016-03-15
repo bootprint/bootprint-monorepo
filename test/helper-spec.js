b7f76c0e17928d4ddc99f93e06fa737b4e36c2b0
Nils Knappmeier <npm@knappi.org>
2016-03-15 12:25:56 +0100

    Adjust travis-configuration

--- test/helper-spec.js
+++ test/helper-spec.js
@@ -1,15 +1,17 @@
 /* global describe */
 /* global it */
-/* global expect */
+
+var expect = require('chai').expect
+
 var helpers = require('../handlebars/helpers.js')
 describe("the 'htmlId'-helper'", function () {
   it("should replace illegal characters with '-'", function () {
-    expect(helpers.htmlId(';/abc')).toBe('--abc')
+    expect(helpers.htmlId(';/abc')).to.equal('--abc')
   })
 })
 
 describe("the 'toUpperCase'-helper'", function () {
   it('should convert strings to upper-case', function () {
-    expect(helpers.toUpperCase('abc')).toBe('ABC')
+    expect(helpers.toUpperCase('abc')).to.equal('ABC')
   })
 })
