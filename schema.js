65ff2bb8e73b6ec560cb3d6b8e3808dd9366d9a6
Nils Knappmeier <github@knappi.org>
2016-12-19 12:06:19 +0100

    Fix configuration-schema

--- schema.js
+++ schema.js
@@ -3,33 +3,31 @@
  */
 module.exports = {
   description: 'The configuration schema of the "customize-engine-less"',
-  definitions: {
-    'stringArray': {
-      type: 'array',
-      items: {
-        type: 'string'
-      }
-    }
-  },
+  type: 'object',
   properties: {
-    'less': {
-      type: 'object',
-      properties: {
-        'main': {
-          description: 'A list of imported {less}-files',
-          anyOf: [
-            {type: 'string'},
-            {$ref: '#/definitions/stringArray'}
-          ]
-        },
-        'paths': {
-          description: 'A list of directories to be used as {less}-include paths',
-          anyOf: [
-            {type: 'string'},
-            {$ref: '#/definitions/stringArray'}
-          ]
+    'main': {
+      description: 'A list of imported {less}-files',
+      anyOf: [
+        {type: 'string'},
+        {
+          type: 'array',
+          items: {
+            type: 'string'
+          }
         }
-      }
+      ]
+    },
+    'paths': {
+      description: 'A list of directories to be used as {less}-include paths',
+      anyOf: [
+        {type: 'string'},
+        {
+          type: 'array',
+          items: {
+            type: 'string'
+          }
+        }
+      ]
     }
   }
 }
