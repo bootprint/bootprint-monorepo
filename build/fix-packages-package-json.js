a7b541f4d964e372f36d389e2f6101cf8218fac2
Nils Knappmeier <github@knappi.org>
2019-12-31 00:23:59 +0100

    chore: update package.json metadata

--- build/fix-packages-package-json.js
+++ build/fix-packages-package-json.js
@@ -7,14 +7,20 @@ const cp = require('child_process')
 process.chdir(path.resolve(__dirname, '..'))
 
 fs.readdirSync('packages').forEach(pkg => {
-  const packageJsonPath = path.resolve('packages', pkg, 'package.json')
+  const packagePath = path.resolve('packages', pkg)
+  const packageJsonPath = path.resolve(packagePath, 'package.json')
   const originalContents = fs.readFileSync(packageJsonPath, 'utf8')
   const packageJson = JSON.parse(originalContents)
-  updatePeerDependenciesBasedOnDevDependencies(packageJson)
+  fixPackageJson(packageJson, packagePath)
   const newContents = JSON.stringify(packageJson, 0, 2) + '\n'
   fs.writeFileSync(packageJsonPath, newContents)
   cp.spawnSync('git', ['add', packageJsonPath], { stdio: 'inherit' })
 })
+
+function fixPackageJson(packageJson, packagePath) {
+  updatePeerDependenciesBasedOnDevDependencies(packageJson)
+  fixGithubUrls(packageJson, packagePath)
+}
 
 function updatePeerDependenciesBasedOnDevDependencies(packageJson) {
   const devDependencies = packageJson.devDependencies
@@ -27,4 +33,14 @@ function updatePeerDependenciesBasedOnDevDependencies(packageJson) {
       peerDependencies[dependencyName] = devDependencies[dependencyName]
     }
   })
+}
+
+function fixGithubUrls(packageJson, packagePath) {
+  const { repository, bugs, author } = require('../package')
+  packageJson.repository = {
+    ...repository,
+    directory: path.posix.relative(process.cwd(), packagePath)
+  }
+  packageJson.bugs = bugs
+  packageJson.author = author
 }
